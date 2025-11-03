b47d529ccd2320b802eae4a38ced9e92908bb125
Changbin Park <gh4ck3r@gmail.com>
2025-11-03 20:40:00 +0900

    prevent wrapscan

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,17 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- prevent reindent when saving
-vim.g.autoformat = false
-
+local o = vim.opt
 -- keep current line in window when splitting shrinks it
-vim.o.splitkeep = "cursor"
-
+o.splitkeep = "cursor"
 -- Use system clipboard when yank visual block
-vim.opt.clipboard = "unnamedplus"
+o.clipboard = "unnamedplus"
+-- Disable wrapscan
+o.wrapscan = false 
+
+local g = vim.g
+-- prevent reindent when saving
+g.autoformat = false
 
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_lsp = "basedpyright"
+g.lazyvim_python_lsp = "basedpyright"
 -- Set to "ruff_lsp" to use the old LSP implementation version.
-vim.g.lazyvim_python_ruff = "ruff"
+g.lazyvim_python_ruff = "ruff"
