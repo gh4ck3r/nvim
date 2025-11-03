50c1f2aa4ce04747f186504a0659ff0492b44989
Changbin Park <gh4ck3r@gmail.com>
2025-11-03 20:43:56 +0900

    [interestingwords] separate config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,18 +1,2 @@
 return {
-  { -- highlight interesting words
-    "Mr-LLLLL/interestingwords.nvim",
-    opts = {
-      options = {
-        colors = { "#aeee00", "#ff0000", "#0000ff", "#b88823", "#ffa724", "#ff2c4b" },
-        search_count = true,
-        navigation = true,
-        scroll_center = true,
-        search_key = "<leader>m",
-        cancel_search_key = "<leader>M",
-        color_key = "<leader>k",
-        cancel_color_key = "<leader>K",
-        select_mode = "loop", -- random or loop
-      },
-    },
-  },
 }
